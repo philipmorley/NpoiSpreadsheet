9539a05efbffee36afa3635807a540092666ed72
Philip Morley <philip.morley@codurance.com>
2025-09-15 15:42:49 +0100

    Initial commit

---
+++ NpoiSpreadsheet/Program.cs
@@ -0,0 +1,24 @@
+﻿using NPOI.SS.UserModel;
+using System.Reflection;
+
+using var stream = GetTemplateStream();
+using var workbook = WorkbookFactory.Create(stream);
+
+var sheet = workbook.GetSheetAt(0);
+
+for (var i = 1; i <= 34; i++)
+{
+    sheet.AutoSizeColumn(i);
+}
+
+return;
+
+static Stream? GetTemplateStream()
+{
+    var assembly = Assembly.GetExecutingAssembly();
+    var resourcePath = assembly.GetManifestResourceNames()
+        .Single(str => str.EndsWith("Demo.xlsx"));
+
+    var stream = assembly.GetManifestResourceStream(resourcePath);
+    return stream;
+}
